801ffc3704950012542a4c129ccadfd5d3d77820
ItsMeIns <heavyins@gmail.com>
2023-04-20 18:55:16 +0300

    Add TableViewDelegate methods (add and remove checkmark when clicking on a cell)

--- ToDoMore/ToDoListViewController.swift
+++ ToDoMore/ToDoListViewController.swift
@@ -16,6 +16,7 @@ class ToDoListViewController: UITableViewController {
         
     }
 
+    //MARK: - TableViewDataSource -
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         itemArray.count
     }
@@ -24,6 +25,19 @@ class ToDoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+    }
+    
+    //MARK: - TableViewDelegate -
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            print("\(itemArray[indexPath.row]) delete checkmark.")
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            print("\(itemArray[indexPath.row]) add checkmark.")
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
