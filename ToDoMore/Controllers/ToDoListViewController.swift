9da3bc063e018e47bab17160ea8b284220ee5674
ItsMeIns <heavyins@gmail.com>
2023-04-29 17:03:28 +0300

    test removing data from core data

--- ToDoMore/Controllers/ToDoListViewController.swift
+++ ToDoMore/Controllers/ToDoListViewController.swift
@@ -40,6 +40,10 @@ class ToDoListViewController: UITableViewController {
     
     //MARK: - TableViewDelegate -
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
