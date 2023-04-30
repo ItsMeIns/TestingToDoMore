6d04c0a12a2c86433ff2b5dfbeba535737aa5186
ItsMeIns <heavyins@gmail.com>
2023-04-30 16:29:19 +0300

    Added a method with which the information in the list will be returned if nothing is written in the search bar

--- ToDoMore/Controllers/ToDoListViewController.swift
+++ ToDoMore/Controllers/ToDoListViewController.swift
@@ -104,8 +104,15 @@ class ToDoListViewController: UITableViewController, UISearchBarDelegate {
         request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         loadItems(with: request)
-        
-        
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
     }
     
 }
